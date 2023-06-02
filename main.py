c4b0496004e90569403db6805dda45f7f35e5f07
Tobias Legenden <tobiaslegenden@gmail.com>
2023-06-02 12:01:20 +0200

    query working

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import sqlite3
 
-from dash import dash, html, dcc, Output, Input
+from dash import dash, html, dcc, Output, Input, State
 from dash.exceptions import PreventUpdate
 
 
@@ -29,10 +29,12 @@ conn = sqlite3.connect("database/database.db")
 cursor = conn.cursor()
 
 df1.to_sql("games", conn, if_exists="replace", index=False)
-df2.to_sql("adaptions", conn, if_exists="replace", index=False)
+df2.to_sql("movies", conn, if_exists="replace", index=False)
 
 df_sql_games = pd.read_sql_query("SELECT * FROM games", conn)
-df_sql_movie = pd.read_sql_query("SELECT * FROM adaptions", conn)
+df_sql_movie = pd.read_sql_query("SELECT * FROM movies", conn)
+
+conn.close()
 
 # creates the dash application opject
 app = dash.Dash(__name__)
@@ -49,7 +51,25 @@ app.layout = dbc.Container(
     [
         dbc.Row(
             [
-                dbc.Button("Load Data", id="load-btn", n_clicks=0,)
+                dbc.Button("SQL Query", id="load-btn", n_clicks=0,),
+                html.Div(
+                    [
+                        html.H1("Search"),
+                        html.Div(
+                            [
+                                dcc.Textarea(
+                                    id="textarea",
+                                    value="Search...",
+                                ),
+                                html.Button(
+                                    "Search",
+                                    id="sql-search",
+                                    n_clicks=0,
+                                )
+                            ]
+                        ),
+                    ]
+                )
             ]
         ),
         dbc.Row(
@@ -59,19 +79,36 @@ app.layout = dbc.Container(
                         html.H1("Popular Video Games 1980 - 2023, and movie adaptions"),
                         dag.AgGrid(
                             id="ag_grid",
-                            columnDefs=[],
-                            rowData=[],
+                            columnDefs=[{"field": col} for col in df_sql_games.columns],
+                            rowData=df_sql_games.to_dict("records"),
                             defaultColDef={
                                 "minWidth": 100,
                                 "editable": True,
                                 "resizable": True,
                                 "flex": 1,
                                 "wrapHeaderText": True,
+                                "filter": True,
+                                "floatingFilter": True
                             }
                         ),
                         html.H1("Film Adaptations of Video Games"),
                         dag.AgGrid(
                             id="ag_grid-2",
+                            columnDefs=[{"field": col} for col in df_sql_movie.columns],
+                            rowData=df_sql_movie.to_dict("records"),
+                            defaultColDef={
+                                "minWidth": 100,
+                                "editable": True,
+                                "resizable": True,
+                                "flex": 1,
+                                "wrapHeaderText": True,
+                                "filter": True,
+                                "floatingFilter": True
+                            }
+                        ),
+                        html.H1("SQL QUERY"),
+                        dag.AgGrid(
+                            id="ag-sql",
                             columnDefs=[],
                             rowData=[],
                             defaultColDef={
@@ -80,8 +117,11 @@ app.layout = dbc.Container(
                                 "resizable": True,
                                 "flex": 1,
                                 "wrapHeaderText": True,
+                                "filter": True,
+                                "floatingFilter": True
                             }
                         ),
+
                     ]
                 )
             ]
@@ -92,24 +132,22 @@ app.layout = dbc.Container(
 # callbacks
 @app.callback(
     [
-        Output(component_id="ag_grid", component_property="rowData"),
-        Output(component_id="ag_grid", component_property="columnDefs"),
-        Output(component_id="ag_grid-2", component_property="rowData"),
-        Output(component_id="ag_grid-2", component_property="columnDefs"),
+        Output(component_id="ag-sql", component_property="rowData"),
+        Output(component_id="ag-sql", component_property="columnDefs"),
     ],
-    [
-        Input(component_id="load-btn", component_property="n_clicks")
-    ]
+    Input(component_id="sql-search", component_property="n_clicks"),
+    State(component_id="textarea", component_property="value")
 )
-def update_output(n_clicks):
-    if n_clicks <= 0:
-        raise PreventUpdate
-    else:
-        rowData_games = df_sql_games.to_dict("records")
-        columnDefs_games = [{"field": col} for col in df_sql_games.columns]
-        rowData_movie = df_sql_movie.to_dict("records")
-        columnDefs_movie = [{"field": col} for col in df_sql_movie.columns]
-    return rowData_games, columnDefs_games, rowData_movie, columnDefs_movie
+def sql_query(n_clicks, textarea):
+    conn = sqlite3.connect("database/database.db")
+    df_sql_games = pd.read_sql_query(f"SELECT title FROM games WHERE title LIKE '%{textarea}'", conn)
+    query = df_sql_games.to_dict("records")
+    print("df_sql_games: ", df_sql_games)
+
+    columnDefs = [{"field": col} for col in df_sql_games.columns]
+    conn.close()
+
+    return query, columnDefs
 
 if __name__ == '__main__':
     app.run_server(debug=True)
